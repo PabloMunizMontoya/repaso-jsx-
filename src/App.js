7d1245fe327886cb6f68e96008c482a304339238
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-07-26 16:39:20 -0500

    useeffect

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import HolaMundo from "./components/HolaMundo";
 import AdiosMundo from "./components/AdiosMundos";
 import Saludar from "./components/Saludar";
 import Saludar2 from "./components/Saludar2";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
     const [stateCar, setStateCar] = useState(false);
+    const [contar, setContar] = useState(0);
+
+    useEffect(() => {
+        console.log(contar);
+    }, [contar]);
+
     const user = {
         nombre: "Pablo Muñiz Montoya",
         edad: "36",
@@ -17,9 +23,12 @@ function App() {
         /* console.log("Hola" + nombre + " tiene " + edad + " años"); */
         console.log(`Hola ${nombre}, tiene ${edad} años.`);
     };
+
     const encenderApagar = () => {
         setStateCar(!stateCar);
+        setContar(contar + 1);
     };
+
     return (
         <div className="App">
             <header className="App-header">
@@ -29,6 +38,7 @@ function App() {
                 <Saludar userInfo={user} />
                 <Saludar2 saludarFn={saludarFn} userInfo={user} />
                 <h3>El carro esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+                <h4>Clicks: {contar}</h4>
                 <button onClick={encenderApagar}>Encender / Apagar</button>
             </header>
         </div>
