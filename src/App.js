0856d14059acb1243f002ef05d0c3f8847ae7e4e
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-07-26 16:30:17 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import HolaMundo from "./components/HolaMundo";
 import AdiosMundo from "./components/AdiosMundos";
 import Saludar from "./components/Saludar";
 import Saludar2 from "./components/Saludar2";
+import { useState } from "react";
 function App() {
+    const [stateCar, setStateCar] = useState(false);
     const user = {
         nombre: "Pablo Muñiz Montoya",
         edad: "36",
@@ -13,8 +15,10 @@ function App() {
 
     const saludarFn = (nombre, edad) => {
         /* console.log("Hola" + nombre + " tiene " + edad + " años"); */
-
         console.log(`Hola ${nombre}, tiene ${edad} años.`);
+    };
+    const encenderApagar = () => {
+        setStateCar(!stateCar);
     };
     return (
         <div className="App">
@@ -24,6 +28,8 @@ function App() {
                 <AdiosMundo />
                 <Saludar userInfo={user} />
                 <Saludar2 saludarFn={saludarFn} userInfo={user} />
+                <h3>El carro esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+                <button onClick={encenderApagar}>Encender / Apagar</button>
             </header>
         </div>
     );
